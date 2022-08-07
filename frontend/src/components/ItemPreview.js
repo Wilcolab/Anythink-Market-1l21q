1cb4c466793802d6a19772ab40de8a25153ca4dd
Vishal K <93390689+Vishal-K-988@users.noreply.github.com>
2022-08-07 19:19:47 +0530

    Update ItemPreview.js

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -49,12 +49,7 @@ const ItemPreview = (props) => {
     />
         }
       
-{/* <img
-        alt="item"
-        src={item.image}
-        className="card-img-top item-img"
-        style={{ borderRadius: "20px" }}
-      /> */}
+
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
           <h3 className="card-title">{item.title}</h3>
